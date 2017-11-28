b734e6dba5bf95bd02149f0412d548cc7aaff06b
asryan2 <david.kareni@mail.ru>
2017-11-28 22:57:52 +0400

    added percentCompleted

--- src/Components/PlayerSmall/component.js
+++ src/Components/PlayerSmall/component.js
@@ -3,6 +3,24 @@ import "./Styles/style.css"
 import PlayButton from "../PlayButton/component"
 import StopButton from "../StopButton/component"
 class PlayerSmall extends React.Component {
+  animatePathElement(){
+    this.pathElementAnimation = setInterval(()=>{
+      let marginLeftLength = this.pathElement.style.marginLeft.length;
+      if(marginLeftLength === 0)
+        this.pathElement.style.marginLeft = "0px"
+      let marginLeftInt = parseInt(this.pathElement.style.marginLeft.substring(0, this.pathElement.style.marginLeft.length-2), 10)
+      if(this.pathElement.offsetWidth - this.pathWrapper.offsetWidth + marginLeftInt > -10){
+        this.pathElement.style.marginLeft = marginLeftInt - 15 + "px"
+      }
+
+
+    },800);
+  }
+  unanimatePathElement(){
+
+    clearInterval(this.pathElementAnimation);
+    this.pathElement.style.marginLeft = "0px";
+  }
  render() {
    if(this.props.isPlaying){
      this.style = {
@@ -14,18 +32,25 @@ class PlayerSmall extends React.Component {
       background: "rgba(0,17,56,0.3)"
      }
    }
+   let percentBarStyle={
+     background: "linear-gradient(to right, rgba(0,0,0,0.6) 0%, rgba(0,0,0,0.6) " + Math.round(this.props.percentCompleted*100) +"%, rgba(0,0,0,0)" + Math.round(this.props.percentCompleted*100) + "%)"
+   }
   return (
-    <div id="playerSmall"
+    <div
       style={this.style}
-         className={this.props.className + " unselectable"}>
+         className={this.props.className + " unselectable playerSmall"}>
        <PlayButton
          isPlaying={this.props.isPlaying}
          onPlayPause={() => this.props.onPlayPause(this.props.id)}/>
        <StopButton
          onStopped={() => this.props.onStopped(this.props.id)}/>
-      <div id="playerSmall-path"
-        onClick={() => this.props.onPlayPause(this.props.id)}>
-        <span>{this.props.path}</span>
+      <div className="playerSmall-path"
+        onMouseOver={() => this.animatePathElement.bind(this)()}
+        onMouseOut={() => this.unanimatePathElement.bind(this)()}
+        onClick={() => this.props.onPlayPause(this.props.id)}
+        ref={(el) => this.pathWrapper = el}
+        style={percentBarStyle}>
+        <span ref={(el) => this.pathElement = el}>{this.props.path}</span>
       </div>
     </div>
   );
